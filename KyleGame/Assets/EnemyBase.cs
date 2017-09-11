1aad1b6b87cfcb31bea2abc9f72ac2e3b49481f2
shimitaku <nkc.takumi.shouzui@gmail.com>
2017-09-11 12:07:10 +0900

    Demo調整

---
+++ KyleGame/Assets/EnemyBase.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyBase : MonoBehaviour
+{
+    Vector3 pos;
+    bool pointZ = true;
+    bool pointY = true;
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        towerPosY();
+        towerPosZ();
+        pos = transform.position;
+    }
+    //Y方向に動く(浮遊)
+    public void towerPosY()
+    {
+        if (pointY == true)
+        {
+            transform.position += new Vector3(0, 0.01f, 0);
+            if (pos.y > 24)
+                pointY = false;
+        }
+        else if (pointY == false)
+        {
+            transform.position -= new Vector3(0, 0.01f, 0);
+            if (pos.y < 22)
+                pointY = true;
+        }
+    }
+    //Z方向に動く(ゆらゆら)
+    public void towerPosZ()
+    {
+        if (pointZ == true)
+        {
+            transform.position += new Vector3(0, 0, 0.003f);
+            if (pos.z > 95.5f)
+                pointZ = false;
+        }
+        else if (pointZ == false)
+        {
+            transform.position -= new Vector3(0, 0, 0.003f);
+            if (pos.z < 94.5f)
+                pointZ = true;
+        }
+    }
+}
---
+++ KyleGame/Assets/TimeControl.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class TimeControl : MonoBehaviour {
+
+    public Sprite[] sprite = new Sprite[10]; 
+
+    // Use this for initialization
+    void Start() {
+
+        
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
