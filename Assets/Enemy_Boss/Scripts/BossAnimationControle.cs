6f0868a9681bf6da103f0b178db328467a799219
DeNChi002 <nkc.daiki.fujiwara@gmail.com>
2017-09-13 12:08:37 +0900

    体が硬いボスをほぐして、アニメーションの制御をできるようにした

---
+++ Assets/Enemy_Boss/Scripts/BossAnimationControle.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossAnimationControle : MonoBehaviour
+{
+    Animator BossAnimator;
+
+    float debug_speed = 0f;
+
+    void Start ()
+    {
+        BossAnimator = GetComponent<Animator>();
+	}
+	
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.V))
+        {
+            Animation_Death();
+        }
+
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            if (BossAnimator.GetBool("Beam"))
+            {
+                Animation_Beam(false);
+            }
+            else
+            {
+                Animation_Beam(true);
+            }            
+        }
+
+        if (Input.GetKey(KeyCode.N))
+        {
+            debug_speed += 0.01f;
+
+            Mathf.Clamp(debug_speed, 0f, 1f);
+
+            Animation_Move(debug_speed);
+        }
+    }
+
+    /// <summary>
+    /// アニメーション：移動
+    /// </summary>
+    /// <param name="speed">移動量</param>
+    public void Animation_Move(float speed)
+    {
+        BossAnimator.SetFloat("Speed", speed);
+    }
+
+    /// <summary>
+    /// アニメーション：ビーム攻撃
+    /// </summary>
+    /// <param name="state">ステート</param>
+    public void Animation_Beam(bool parametor)
+    {
+        BossAnimator.SetBool("Beam", parametor);
+    }
+
+    /// <summary>
+    /// アニメーション：死亡
+    /// </summary>
+    public void Animation_Death()
+    {
+        BossAnimator.SetTrigger("Death");
+    }
+}
