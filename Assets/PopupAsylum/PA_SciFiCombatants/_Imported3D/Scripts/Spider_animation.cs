9050dcd2cd81e365b3d29f033495226e1592ae2f
shimitaku <nkc.takumi.shouzui@gmail.com>
2017-09-15 11:55:13 +0900

    Spiderの攻撃EF追加

---
+++ Assets/PopupAsylum/PA_SciFiCombatants/_Imported3D/Scripts/Spider_animation.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spider_animation : MonoBehaviour {
+
+    public Animator spider_animator;
+    public ParticleSystem spark;
+
+    private float speed;
+ 
+    // Use this for initialization
+    void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //ここは任意に使って
+        speed = spider_animator.GetFloat("Speed");
+
+        if (speed >= 1.0f)
+            Walk();
+       
+	}
+    //移動
+    void Walk()
+    {
+        spider_animator.Play("Walk");
+    }
+
+    //攻撃
+    void Attack()
+    {
+        spider_animator.SetTrigger("Attack");
+        spark.Play();
+    }
+}
