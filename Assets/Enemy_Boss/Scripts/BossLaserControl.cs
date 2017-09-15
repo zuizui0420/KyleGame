7877f32f002c3cc918c64378f17eac75256d30dc
shimitaku <nkc.takumi.shouzui@gmail.com>
2017-09-15 10:09:16 +0900

    bossにlaser搭載

---
+++ Assets/Enemy_Boss/Scripts/BossLaserControl.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossLaserControl : MonoBehaviour {
+
+    public GameObject eyesPos,targetPos,laser;//Laserの発射座標と着弾座標
+    public bool fire;
+  
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        //レーザー照射
+        if (fire)
+        {
+            Instantiate(laser, transform.position, Quaternion.identity);
+            fire = false;
+          
+        }
+        //照射していない時
+        else
+        {
+            
+        }
+
+
+    }
+
+    private void FixedUpdate()
+    {
+        RaycastHit hitInfo;//ヒットしたオブジェクトの情報
+        if(fire)
+        Physics.Raycast(eyesPos.transform.position, targetPos.transform.position - eyesPos.transform.position,out hitInfo);
+    }
+}
---
+++ Assets/Enemy_Boss/Scripts/Laser.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour {
+    public GameObject target;
+
+    private float life_time = 1.0f;
+    private Vector3 move_pos;
+  
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        target = GameObject.Find("Target_boss");
+
+
+        transform.position = Vector3.Lerp(transform.position,target.transform.position,0.2f);
+        
+        Destroy(this.gameObject,life_time);
+
+    }
+}
