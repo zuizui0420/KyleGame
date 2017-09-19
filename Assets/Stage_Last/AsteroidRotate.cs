2957316c3ecf1fde30bef5e14a7de86dd582a4ed
DeNChi002 <nkc.daiki.fujiwara@gmail.com>
2017-09-19 11:11:05 +0900

    ボスステージを修正・ドア未設置

---
+++ Assets/Stage_Last/AsteroidRotate.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// アステロイド群を回転する
+/// </summary>
+public class AsteroidRotate : MonoBehaviour
+{
+	void Update ()
+    {
+        Vector3 Rot = transform.eulerAngles;
+        Rot.y += 0.1f;
+        transform.eulerAngles = Rot;
+	}
+}
