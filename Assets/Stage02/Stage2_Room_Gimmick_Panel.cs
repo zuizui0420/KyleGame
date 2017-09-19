c009e0e06fda322f76f4a9ce3724506edf11c00d
DeNChi002 <nkc.daiki.fujiwara@gmail.com>
2017-09-19 18:26:21 +0900

    ギミックの作成

---
+++ Assets/Stage02/Stage2_Room_Gimmick_Panel.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stage2_Room_Gimmick_Panel : MonoBehaviour
+{
+    [SerializeField, Header("ID")]
+    public int ID;
+}
