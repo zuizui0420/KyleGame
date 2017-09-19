ba7290fcd7496ec2624cd0464a477032515e02ca
shimitaku <nkc.takumi.shouzui@gmail.com>
2017-09-19 09:46:08 +0900

    EF追加

---
+++ Assets/EF/Exprotion01.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exprotion01 : MonoBehaviour {
+    public GameObject spider;
+    public ParticleSystem particle;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
