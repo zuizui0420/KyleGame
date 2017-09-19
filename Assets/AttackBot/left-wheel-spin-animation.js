4c95f75032820b9c81c2e230cf068e03c7a3a67c
DeNChi002 <nkc.daiki.fujiwara@gmail.com>
2017-09-19 16:36:12 +0900

    スイッチギミックの作成・不要なアセットを削除

--- Assets/AttackBot/left-wheel-spin-animation.js
+++
@@ -1,6 +0,0 @@
-﻿function Update() {
-		// Slowly rotate the object around its X axis at 1 degree/second.
-			transform.Rotate(0, Time.deltaTime*90, 0);
-
-		
-	}
---
+++ Assets/Stage02/Stage02_Room_Gimmick.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// STAGE2_ROOM02 ギミック：電気床
+/// </summary>
+public class Stage02_Room_Gimmick : MonoBehaviour
+{
+	void Start ()
+    {
+		
+	}
+
+	void Update ()
+    {
+		
+	}
+}
