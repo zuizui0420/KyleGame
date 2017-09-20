c1c69c8fcb50089f149a0c35c99b0bcc27bff7e6
DeNChi002 <nkc.daiki.fujiwara@gmail.com>
2017-09-20 10:31:19 +0900

    ステージギミックの作成中

---
+++ Assets/Stage02/SCI_FI_Barrel/Scripts/SCI_FI_Motor.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SCI_FI_Motor : MonoBehaviour
+{
+    [SerializeField, Header("Barrel")]
+    GameObject Barrel;
+
+    [SerializeField, Header("回転中かどうか")]
+    public bool SpinOn;
+
+    [SerializeField, Header("回転速度")]
+    public float SpinSpeed;
+
+	private void Update ()
+    {
+        if (SpinOn)
+        {
+            Vector3 Rot = Barrel.transform.localEulerAngles;
+            Rot.y += SpinSpeed;
+            Barrel.transform.localEulerAngles = Rot;
+        }
+        else
+        {
+            if(SpinSpeed > 0)
+            {
+                Vector3 Rot = Barrel.transform.localEulerAngles;
+                Rot.y += SpinSpeed;
+                Barrel.transform.localEulerAngles = Rot;
+
+                SpinSpeed -= 0.1f;
+            }
+            else
+            {
+                SpinSpeed = 0f;
+            }
+        }      
+    }
+}
---
+++ Assets/Stage02/SCI_FI_Barrel/Scripts/SCI_FI_WallLamp.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SCI_FI_WallLamp : MonoBehaviour
+{
+    [SerializeField, Header("Barrel")]
+    GameObject Barrel;
+
+    [SerializeField, Header("色が切り替わる速度"), Range(0.1f, 1f)]
+    float ColorLerpSpeed;
+
+    [SerializeField, Header("回転速度"), Range(0f, 1f)]
+    float RotateSpeed;
+
+    [SerializeField, Header("")]
+    Color DefaultEmissionColor;
+
+    [SerializeField]
+    Color MoveEmissionColor;
+
+    MeshRenderer m_renderer;
+
+    float currentColorTime = 0f;
+
+    Color color;
+
+    void Start()
+    {
+        m_renderer = Barrel.GetComponent<MeshRenderer>();
+
+        //EmissionColorの使用を可能にする
+        m_renderer.material.EnableKeyword("_EMISSION");
+
+        //初期EmissionColorを設定
+        m_renderer.material.SetColor("_EmissionColor", DefaultEmissionColor);
+    }
+
+    void Update()
+    {
+        //カラー遷移
+        ColorMove();
+
+        //Y軸回転
+        Vector3 Rot = Barrel.transform.localEulerAngles;
+        Rot.y += RotateSpeed;
+        Barrel.transform.localEulerAngles = Rot;
+    }
+
+    private void ColorMove()
+    {
+        currentColorTime += Time.deltaTime;
+
+        if (currentColorTime > 4f)
+        {
+            currentColorTime = 0f;
+        }
+        else if (currentColorTime > 2f)
+        {
+            color = Color.Lerp(DefaultEmissionColor, MoveEmissionColor, ColorLerpSpeed);
+        }
+        else
+        {
+            color = Color.Lerp(MoveEmissionColor, DefaultEmissionColor, ColorLerpSpeed);
+        }
+
+        m_renderer.material.SetColor("_EmissionColor", color);
+    }
+}
