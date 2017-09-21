5d47c41ba9672ffc2ebe73e99ee8b48eb70511ed
shimitaku <nkc.takumi.shouzui@gmail.com>
2017-09-21 14:50:05 +0900

    life追加

---
+++ Assets/Scripts/UI/BatteryControl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BatteryControl : MonoBehaviour {
+
+    private float battery = 1;
+    private float G, B;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void BatteryUse()
+    {
+        float startTime = Time.timeSinceLevelLoad;
+        while (true)
+        {
+            float time = (Time.timeSinceLevelLoad - startTime);
+            if (time >= 100.0f) break;            
+            battery = time / 100;
+            GetComponent<Image>().color = new Color(1, battery, battery, 1);
+        }
+    }
+}
