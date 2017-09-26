ea3fb62b93efe8ae30ed9861c8d1f814ae3ee34f
shimitaku <nkc.takumi.shouzui@gmail.com>
2017-09-26 12:47:34 +0900

    SE追加

---
+++ Assets/Scripts/Sound/LaserSoundControl.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserSoundControl : MonoBehaviour {
+
+    [SerializeField] private AudioSource[] audioSources = new AudioSource[2];
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+            StartCoroutine("LaserSoundPlay");
+		
+	}
+
+    private IEnumerator LaserSoundPlay()
+    {
+        float startTime = Time.timeSinceLevelLoad;
+        audioSources[0].Play();
+
+        while (true)
+        {
+            float time = Time.timeSinceLevelLoad - startTime;
+            if(time >= 2)
+            {
+                audioSources[1].Play();
+                break;
+            }
+        }
+
+        yield return null;
+    }
+}
