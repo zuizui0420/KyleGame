b6c36a673b2ecdd996d18b2c224ff5c27893e24d
shimitaku <nkc.takumi.shouzui@gmail.com>
2017-09-26 13:41:26 +0900

    no message

--- Assets/Scripts/Sound/LaserSoundControl.cs
+++ Assets/Scripts/Sound/LaserSoundControl.cs
@@ -13,8 +13,8 @@ public class LaserSoundControl : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        
-            StartCoroutine("LaserSoundPlay");
+
+        StartCoroutine("LaserSoundPlay");
 		
 	}
 
