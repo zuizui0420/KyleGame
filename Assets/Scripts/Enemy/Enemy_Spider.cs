615103da9ec85846be73a3d3c5beb10124e7479c
DeNChi002 <nkc.daiki.fujiwara@gmail.com>
2017-09-27 12:29:17 +0900

    敵：スパイダーの制御スクリプトを作成

---
+++ Assets/Scripts/Enemy/Enemy_Spider.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 敵：スパイダー
+/// </summary>
+public class Enemy_Spider : EnemyBase
+{
+    [SerializeField, Header("敵の体力(０の場合は無敵)")]
+    float Life;
+
+	void Start ()
+    {
+        LIFE = Life;	
+	}
+	
+    /// <summary>
+    /// 敵：死亡処理
+    /// </summary>
+    protected override void EnemyDead()
+    {
+        base.EnemyDead();
+    }
+}
