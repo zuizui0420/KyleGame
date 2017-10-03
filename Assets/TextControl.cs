f4d73b4be94355d2f1fd40a6ef10190cdacc157e
shimitaku <nkc.takumi.shouzui@gmail.com>
2017-10-03 09:43:28 +0900

    UIテキスト

--- Assets/TextControl.cs
+++ Assets/TextControl.cs
@@ -1,18 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TextControl : MonoBehaviour {
 
-    [SerializeField] Canvas[] canvas = new Canvas[3];
+    [SerializeField] GameObject[] canvas = new GameObject[3];
+    
+    private int canvasCount = 0;
 
-	// Use this for initialization
-	void Start () {
-		
+
+    // Use this for initialization
+    void Start () {
+        
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+
+    public void DisplayText()
+    {
+        canvas[canvasCount].SetActive(true);
+
+        Time.timeScale = 0;
+    }
+
+    public void HideText()
+    {
+        canvas[canvasCount].SetActive(false);
+        canvasCount++;
+
+        Time.timeScale = 1;
+    }
 }
